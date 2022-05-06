d2cf4ff9d96740ef11f9a22e88b6a97156b7d994
Arian <arian_febrian_29rpl@student.smktelkom-mlg.sch.id>
2022-05-06 21:16:02 +0700

    Lesson/23

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -11,9 +11,14 @@ class _ChooseLocationState extends State<ChooseLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(
-      'Choose location screen!'
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location!'),
+        centerTitle: true,
+        elevation: 0,
       ),
+      body: Text('Choose location screen!'),
     );
   }
 }
