39347c2c45b412ae088637dedae8b239a4b1d0cf
Arian <arian_febrian_29rpl@student.smktelkom-mlg.sch.id>
2022-05-08 12:43:44 +0700

    Lesson/30

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,25 +1,43 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
   @override
-  State<Home> createState() => _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+
+  Map data = {};
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    try {
+      data = ModalRoute.of(context)!.settings.arguments as Map;
+    }
+    catch (e) {
+      data = data;
+    }
+    print(data);
+
     return Scaffold(
+      //appBar: AppBar(),
       body: SafeArea(
         child: Column(
           children: <Widget>[
             FlatButton.icon(
-              onPressed: (){
-                Navigator.pushNamed(context, '/location');
-              },
-              icon: Icon(Icons.edit_location),
-              label: Text('Edit Location'),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+                },
+                icon: Icon(Icons.edit_location),
+                label: Text(
+                    'Edit Location'
+                )
             ),
           ],
         ),
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -8,14 +8,14 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  String time = 'loading';
 
   void setupWorldTime() async {
     WorldTime instance = WorldTime(location: 'Jakarta', flag: 'indonesia.png', url: 'Asia/Jakarta');
     await instance.getTime();
-    print(instance.time);
-    setState(() {
-      time = instance.time;
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
     });
   }
 
@@ -30,7 +30,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(50.0),
-        child: Text(time),
+        child: Text('loading...'),
       ),
     );
   }
